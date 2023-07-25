1d89cb12084afddd558f13353bf0159a88b04429
DmytroAntoniuk <8antoniuk8@gmail.com>
2023-07-25 18:24:32 +0300

    create console crash fixed

--- include/logger-configurator.h
+++ include/logger-configurator.h
@@ -13,6 +13,6 @@ namespace Brand
         constexpr size_t max_log_file_size  = 100 * 1024 * 1024;
         spdlog::level::level_enum log_level = spdlog::level::debug;
 
-        Logger::Configurator::Configure({.logs_dir_path = Brand::GetLogsDirPath(), .log_level = log_level, .max_log_file_size = max_log_file_size, .create_console = true});
+        Logger::Configurator::Configure({.logs_dir_path = Brand::GetLogsDirPath(), .log_level = log_level, .max_log_file_size = max_log_file_size, .create_console = false});
     }
 }  // namespace Brand
